d96d05f45260edc34a8efeade15efcdbd65f2e41
KT 2022 <103476893+kt946@users.noreply.github.com>
2022-07-15 21:16:35 -0400

    add function to delete notes from db.json

--- server.js
+++ server.js
@@ -28,12 +28,43 @@ function createNewNote(body, notesArray) {
     return note;
 }
 
+// function to filter notes array by id
+function filterById(id, notesArray) {
+    console.log(`Deleting id: ${id}`);
+    // Parse JSON string and filter array to exclude parameter id
+    const result = JSON.parse(notesArray).filter(note => note.id !== id);
+    return result;
+}
+
+// function to delete notes
+function deleteNote(req) {
+    // read file for existing array of notes
+    fs.readFile(path.join(__dirname, './db/db.json'), 'utf8', (err, data) => {
+        if (err) throw err;
+
+        // filter current array for parameter and return updated array
+        const updatedNotesArray = filterById(req.params.id, data);
+
+        // convert array to JSON string and write updated array back to db.json
+        fs.writeFile(
+            path.join(__dirname, './db/db.json'),
+            JSON.stringify(updatedNotesArray, null, 2),
+            (writeErr) =>
+                writeErr
+                    // if error, print error message
+                    ? console.error(writeErr)
+                    // if successful, print success message
+                    : console.info('Successfully deleted note!')
+        );
+    });
+}
+
 // static route for notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
 });
 
-// static route for db.json
+// static route for db.json and return saved notes
 app.get('/api/notes', (req, res) => res.json(notes));
 
 // static route for index.html, * should always come last
@@ -41,7 +72,7 @@ app.get('*', (req, res) => {
     res.sendFile(__dirname + '/public/index.html');
 });
 
-// route to POST requests to notes
+// route for POST requests to notes
 app.post('/api/notes', (req, res) => {
     // create new id based on next index in array
     req.body.id = notes.length.toString();
@@ -51,6 +82,13 @@ app.post('/api/notes', (req, res) => {
     
     // return new note
     res.json(newNote);
+});
+
+// route for DELETE requests to notes
+app.delete('/api/notes/:id', (req, res) => {
+    const newNoteArray = deleteNote(req);
+
+    res.json(newNoteArray);
 });
 
 app.listen(PORT, () => {
